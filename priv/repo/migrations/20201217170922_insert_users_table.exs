310ece15600fc2bbda3ed323541f2c8ff2a7dc3f
Hugo Fief <hfief1806@gmail.com>
2020-12-17 18:32:43 +0100

    add migration file execute insert users

---
+++ priv/repo/migrations/20201217170922_insert_users_table.exs
@@ -0,0 +1,10 @@
+defmodule KiraBijoux.Repo.Migrations.InsertUsersTable do
+  use Ecto.Migration
+
+  def change do
+    execute "INSERT INTO user_roles (id, role, inserted_at, updated_at) VALUES (1, 'user', '17-12-2020', '18-12-2020')"
+    execute "INSERT INTO users (firstname, lastname, mail, password, tel, user_role_id, inserted_at, updated_at) VALUES ('test', 'test', 'test@test.com', 'test', '0634896623', 1, '12-12-2020', '12-12-2020')"
+    execute "INSERT INTO users (firstname, lastname, mail, password, tel, user_role_id, inserted_at, updated_at) VALUES ('toto', 'toto', 'toto@toto.com', 'toto', '0634896623', 1, '13-12-2020', '13-12-2020')"
+    execute "INSERT INTO users (firstname, lastname, mail, password, tel, user_role_id, inserted_at, updated_at) VALUES ('tonton', 'tonton', 'tonton@tonton.com', 'tonton', '0634896623', 1, '14-12-2020', '14-12-2020')"
+  end
+end
