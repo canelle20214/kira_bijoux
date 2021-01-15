d712d11426f9fcb675566e62ef440b09b5340580
Hugo Fief <hfief1806@gmail.com>
2021-01-15 18:27:45 +0100

    fix error jason_encoder

--- lib/jason_encoder.ex
+++ lib/jason_encoder.ex
@@ -3,31 +3,27 @@ alias KiraBijoux.Item
 
 # lib/jason_encoder.ex
 defimpl Jason.Encoder, for: User do
-  def encode(%{__struct__: _} = struct, options) do
-    map =
-      struct
-      |> Map.from_struct()
-      |> sanitize_map
+    def encode(%{__struct__: _} = struct, options) do
+        map = struct
+        |> Map.from_struct
+        |> sanitize_map
+        Jason.Encoder.Map.encode(map, options)
+    end
 
-    Jason.Encoder.Map.encode(map, options)
-  end
-
-  defp sanitize_map(map) do
-    Map.drop(map, [:__meta__, :__struct__])
-  end
+    defp sanitize_map(map) do
+        Map.drop(map, [:__meta__, :__struct__])
+    end
 end
 
 defimpl Jason.Encoder, for: Item do
-  def encode(%{__struct__: _} = struct, options) do
-    map =
-      struct
-      |> Map.from_struct()
-      |> sanitize_map
+    def encode(%{__struct__: _} = struct, options) do
+        map = struct
+        |> Map.from_struct
+        |> sanitize_map
+        Jason.Encoder.Map.encode(map, options)
+    end
 
-    Jason.Encoder.Map.encode(map, options)
-  end
-
-  defp sanitize_map(map) do
-    Map.drop(map, [:__meta__, :__struct__])
-  end
+    defp sanitize_map(map) do
+        Map.drop(map, [:__meta__, :__struct__])
+    end
 end
