21a157a2dd220b6f148e3f8710e76912cbac5e2e
Hugo Fief <hfief1806@gmail.com>
2021-01-15 23:06:50 +0100

    remove temp comment

--- lib/kira_bijoux_web/views/item_view.ex
+++ lib/kira_bijoux_web/views/item_view.ex
@@ -3,48 +3,34 @@ defmodule KiraBijouxWeb.ItemView do
 
   def render(conn, "index.json", %{item: item}) do
     conn
-    |> json(item)
+    |> json(item_construction(item))
   end
 
   def render(conn, "index.json", %{items: items}) do
+    items =
+      Enum.map(items, fn n ->
+        item_construction(n)
+      end)
+
     conn
     |> json(items)
   end
+
+  def item_construction(item) do
+    materials = Repo.all(from mi in Material.Item, select: mi, where: mi.item_id == ^item.id)
+    parent = Repo.get!(Item.Parent, item.item_parent_id)
+    type = Repo.get!(Item.Type, parent.item_type_id)
+    collection = Repo.get!(Collection, parent.collection_id)
+
+    Map.new(:name, parent.name)
+    |> Map.put(:description, item.description)
+    |> Map.put(:stock, item.stock)
+    |> Map.put(:price, item.price)
+    |> Map.put(:materials, materials)
+    |> Map.put(:item_type, type)
+    |> Map.put(:collection, collection)
+    |> Map.put(:visibility, item.visibility)
+    |> Map.put(:inserted_at, item.inserted_at)
+    |> Map.put(:updated_at, item.updated_at)
+  end
 end
-
-
-
-
-# defmodule KiraBijouxWeb.ItemView do
-#   use KiraBijouxWeb, :view
-#
-#   def render(conn, "index.json", %{item: item}) do
-#     conn
-#     |> json(item_construction(item))
-#   end
-#
-#   def render(conn, "index.json", %{items: items}) do
-#     items = Enum.map(items, fn n ->
-#       item_construction(n)
-#     end)
-#     conn
-#     |> json(items)
-#   end
-#
-#   def item_construction(item) do
-#     materials = Repo.all(from mi in Material.Item, select: mi, where: mi.item_id == ^item.id)
-#     parent = Repo.get!(Item.Parent, item.item_parent_id)
-#     type = Repo.get!(Item.Type, parent.item_type_id)
-#     collection = Repo.get!(Collection, parent.collection_id)
-#     Map.new(:name, parent.name)
-#     |> Map.put(:description, item.description)
-#     |> Map.put(:stock, item.stock)
-#     |> Map.put(:price, item.price)
-#     |> Map.put(:materials, materials)
-#     |> Map.put(:item_type, type)
-#     |> Map.put(:collection, collection)
-#     |> Map.put(:visibility, item.visibility)
-#     |> Map.put(:inserted_at, item.inserted_at)
-#     |> Map.put(:updated_at, item.updated_at)
-#   end
-# end
