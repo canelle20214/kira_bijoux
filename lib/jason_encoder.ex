06af515ecdb97d080e400477ffeb041907bcda4e
dedede <hifeifi>
2021-01-15 18:08:32 +0100

    test fix erorr jason_encoder

--- lib/jason_encoder.ex
+++ lib/jason_encoder.ex
@@ -1,15 +1,33 @@
 alias KiraBijoux.User
+alias KiraBijoux.Item
 
 # lib/jason_encoder.ex
 defimpl Jason.Encoder, for: User do
-    def encode(%{__struct__: _} = struct, options) do
-        map = struct
-            |> Map.from_struct
-            |> sanitize_map
-        Jason.Encoder.Map.encode(map, options)
-    end
+  def encode(%{__struct__: _} = struct, options) do
+    map =
+      struct
+      |> Map.from_struct()
+      |> sanitize_map
 
-    defp sanitize_map(map) do
-        Map.drop(map, [:__meta__, :__struct__])
-    end
+    Jason.Encoder.Map.encode(map, options)
+  end
+
+  defp sanitize_map(map) do
+    Map.drop(map, [:__meta__, :__struct__])
+  end
+end
+
+defimpl Jason.Encoder, for: Item do
+  def encode(%{__struct__: _} = struct, options) do
+    map =
+      struct
+      |> Map.from_struct()
+      |> sanitize_map
+
+    Jason.Encoder.Map.encode(map, options)
+  end
+
+  defp sanitize_map(map) do
+    Map.drop(map, [:__meta__, :__struct__])
+  end
 end
