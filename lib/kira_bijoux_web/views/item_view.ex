042a7773a8ba5f83e8d9738e1860c9f00848dc06
Hugo FIEF <hugo.fief@dcube.fr>
2021-01-17 23:33:25 +0100

    setting up basic swagger for item_controller

--- lib/kira_bijoux_web/views/item_view.ex
+++ lib/kira_bijoux_web/views/item_view.ex
@@ -1,33 +1,52 @@
 defmodule KiraBijouxWeb.ItemView do
-  use KiraBijouxWeb, :view
+ use KiraBijouxWeb, :view
 
   def render(conn, "index.json", %{item: item}) do
     conn
-    |> json(item_construction(item))
+    |> json(item)
   end
 
   def render(conn, "index.json", %{items: items}) do
-    items = Enum.map(items, fn n ->
-      item_construction(n)
-    end)
     conn
     |> json(items)
   end
-
-  def item_construction(item) do
-    materials = Repo.all(from mi in Material.Item, select: mi, where: mi.item_id == ^item.id)
-    parent = Repo.get!(Item.Parent, item.item_parent_id)
-    type = Repo.get!(Item.Type, parent.item_type_id)
-    collection = Repo.get!(Collection, parent.collection_id)
-    Map.new(:name, parent.name)
-    |> Map.put(:description, item.description)
-    |> Map.put(:stock, item.stock)
-    |> Map.put(:price, item.price)
-    |> Map.put(:materials, materials)
-    |> Map.put(:item_type, type)
-    |> Map.put(:collection, collection)
-    |> Map.put(:visibility, item.visibility)
-    |> Map.put(:inserted_at, item.inserted_at)
-    |> Map.put(:updated_at, item.updated_at)
-  end
 end
+
+### ATTENTION ###
+# TODO pour l'instant ce code ne fonctionne et fait crash entierrement l'api 
+# A décommenter une fois que les fichiers de migrations pour les insert de données 
+# auront été ajoutés dans la base donnée
+
+# defmodule KiraBijouxWeb.ItemView do
+#   use KiraBijouxWeb, :view
+
+#   def render(conn, "index.json", %{item: item}) do
+#     conn
+#     |> json(item_construction(item))
+#   end
+
+#   def render(conn, "index.json", %{items: items}) do
+#     items = Enum.map(items, fn n ->
+#       item_construction(n)
+#     end)
+#     conn
+#     |> json(items)
+#   end
+
+#   def item_construction(item) do
+#     materials = Repo.all(from mi in Material.Item, select: mi, where: mi.item_id == ^item.id)
+#     parent = Repo.get!(Item.Parent, item.item_parent_id)
+#     type = Repo.get!(Item.Type, parent.item_type_id)
+#     collection = Repo.get!(Collection, parent.collection_id)
+#     Map.new(:name, parent.name)
+#     |> Map.put(:description, item.description)
+#     |> Map.put(:stock, item.stock)
+#     |> Map.put(:price, item.price)
+#     |> Map.put(:materials, materials)
+#     |> Map.put(:item_type, type)
+#     |> Map.put(:collection, collection)
+#     |> Map.put(:visibility, item.visibility)
+#     |> Map.put(:inserted_at, item.inserted_at)
+#     |> Map.put(:updated_at, item.updated_at)
+#   end
+# end
