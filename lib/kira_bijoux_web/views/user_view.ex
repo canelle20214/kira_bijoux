2770337a6200d7867daa9c3466d6e6364bf7a219
Hugo FIEF <hugo.fief@dcube.fr>
2021-01-23 15:40:37 +0100

    new view user and item => api

--- lib/kira_bijoux_web/views/user_view.ex
+++ lib/kira_bijoux_web/views/user_view.ex
@@ -1,46 +1,46 @@
-defmodule KiraBijouxWeb.UserView do
- use KiraBijouxWeb, :view
-
-  def render(conn, "index.json", %{user: user}) do
-    conn
-    |> json(user)
-  end
-
-  def render(conn, "index.json", %{users: users}) do
-    conn
-    |> json(users)
-  end
-end
-
-### ATTENTION ###
-# TODO pour l'instant ce code ne fonctionne et fait crash entierrement l'api 
-# A décommenter une fois que les fichiers de migrations pour les insert de données 
-# auront été ajoutés dans la base donnée
-
 # defmodule KiraBijouxWeb.UserView do
-#   use KiraBijouxWeb, :view
+#  use KiraBijouxWeb, :view
 
 #   def render(conn, "index.json", %{user: user}) do
 #     conn
-#     |> json(user_construction(user))
+#     |> json(user)
 #   end
 
 #   def render(conn, "index.json", %{users: users}) do
-#     users = Enum.map(users, & user_construction(&1))
 #     conn
 #     |> json(users)
 #   end
-
-#   def user_construction(user) do
-#     addresses = Repo.all(from ua in User.Address, select: ua, where: ua.user_id == ^user.id)
-#     role = Repo.get!(User.Role, user.user_role_id)
-#     Map.new(:firstname, user.firstname)
-#     |> Map.put(:lastname, user.lastname)
-#     |> Map.put(:mail, user.mail)
-#     |> Map.put(:password, user.password)
-#     |> Map.put(:addresses, addresses)
-#     |> Map.put(:role, role)
-#     |> Map.put(:inserted_at, user.inserted_at)
-#     |> Map.put(:updated_at, user.updated_at)
-#   end
 # end
+
+### ATTENTION ###
+# TODO pour l'instant ce code ne fonctionne et fait crash entierrement l'api
+# A décommenter une fois que les fichiers de migrations pour les insert de données
+# auront été ajoutés dans la base donnée
+
+defmodule KiraBijouxWeb.UserView do
+  use KiraBijouxWeb, :view
+
+  def render(conn, "index.json", %{user: user}) do
+    conn
+    |> json(user_construction(user))
+  end
+
+  def render(conn, "index.json", %{users: users}) do
+    users = Enum.map(users, & user_construction(&1))
+    conn
+    |> json(users)
+  end
+
+  def user_construction(user) do
+    addresses = Repo.all(from ua in User.Address, select: ua, where: ua.user_id == ^user.id)
+    role = Repo.get!(User.Role, user.user_role_id)
+    Map.new(firstname: user.firstname)
+    |> Map.put(:lastname, user.lastname)
+    |> Map.put(:mail, user.mail)
+    |> Map.put(:password, user.password)
+    |> Map.put(:addresses, addresses)
+    |> Map.put(:role, role)
+    |> Map.put(:inserted_at, user.inserted_at)
+    |> Map.put(:updated_at, user.updated_at)
+  end
+end
