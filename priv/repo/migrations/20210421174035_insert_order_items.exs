60778b2f721415fec7b57a1be245f2fff996999e
Hugo Fief <hfief1806@gmail.com>
2021-05-01 20:06:09 +0200

    update migration order & order_item

--- priv/repo/migrations/20210421174035_insert_order_items.exs
+++ priv/repo/migrations/20210421174035_insert_order_items.exs
@@ -3,11 +3,10 @@ defmodule KiraBijoux.Repo.Migrations.InsertOrderItems do
 
   def change do
     Repo.insert_all(Order.Item,
-      [ %{order_id: 4, item_id: 3, quantity: 1, inserted_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second), updated_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)},
-      %{order_id: 4, item_id: 8, quantity: 1, inserted_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second), updated_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)},
-      %{order_id: 4, item_id: 12, quantity: 1, inserted_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second), updated_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)},
-      %{order_id: 5, item_id: 15, quantity: 4, inserted_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second), updated_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)},
-      %{order_id: 5, item_id: 16, quantity: 1, inserted_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second), updated_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)}
+      [ %{order_id: 1, item_id: 1, quantity: 1, inserted_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second), updated_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)},
+      %{order_id: 1, item_id: 7, quantity: 1, inserted_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second), updated_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)},
+      %{order_id: 1, item_id: 14, quantity: 2, inserted_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second), updated_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)},
+      %{order_id: 1, item_id: 16, quantity: 1, inserted_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second), updated_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)}
       ]
     )
   end
